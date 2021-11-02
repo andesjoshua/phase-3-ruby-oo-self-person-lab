bfaf2415a6058a3ce7022288ad67d1e096aa7f19
Joshua Andes <andesjoshua@comcast.net>
2021-11-02 17:13:59 -0400

    commit tree 9eddc139c74c755d674c785c9d0a0a6c321fc761

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
 # your code goes here
+class Person 
+
+attr_accessor :bank_account
+attr_reader :name, :hygiene, :happiness
+
+def initialize(name) 
+    @name = name
+    @hygiene = 5
+    @happiness = 8
+    @bank_account = 25
+end
+
+
+def happiness = (num)
+    @happiness =  num.clamp(0,10)
+end
+
+def hygiene = (num)
+    @hygiene = num.clamp(0,10)
+end
+
+end
